da32b4e504f0c8e9ed21e7067407d983616f58f8
danilo.queiroz <danilo.queiroz@crptecnologia.com.br>
2021-05-04 18:20:00 -0300

    added static charts bar and donut

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 function Footer() {
   return (
-<footer className="footer mt-auto py-3 bg-dark">
-  <div className="container">
-    <p className="text-light">App desenvolvido por <a href="https://github.com/daniloqueiroz-ti" target="_blank" rel="noreferrer">Danilo Queiroz</a></p>
-    <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">App desenvolvido por <a href="https://github.com/daniloqueiroz-ti" target="_blank" rel="noreferrer">Danilo Queiroz</a></p>
+        <p className="text-light"><small><strong>Semana Spring React</strong><br />
       Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
-  </div>
-</footer>
+      </div>
+    </footer>
   );
 }
 
