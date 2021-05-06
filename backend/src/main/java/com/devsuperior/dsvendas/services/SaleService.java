12c1e56c69f158a939e44dea4a4494e695625648
danilo.queiroz <danilo.queiroz@crptecnologia.com.br>
2021-05-05 22:35:43 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperior/dsvendas/services/SaleService.java
@@ -0,0 +1,21 @@
+package com.devsuperior.dsvendas.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dsvendas.entities.Sale;
+import com.devsuperior.dsvendas.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	public List<Sale> findAll(){
+		return repository.findAll();
+	}
+
+}
