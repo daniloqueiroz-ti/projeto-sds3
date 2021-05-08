852cff817efcfdad861e10d541aec719567c0769
danilo.queiroz <danilo.queiroz@crptecnologia.com.br>
2021-05-08 17:12:24 -0300

    First request

--- frontend/src/components/donutchart.tsx
+++ frontend/src/components/donutchart.tsx
@@ -1,5 +1,8 @@
+import axios from 'axios';
 import React from 'react';
 import Chart from 'react-apexcharts'
+import { SaleSum } from 'type/sale';
+import { BASE_URL } from 'utils/requests';
 
 type ChartData = {
   labels: string[],
@@ -11,10 +14,20 @@ function DonutChart() {
   //Forma Errada
   let chartData: ChartData = { labels:[], series: [] }
 
-  const mockData = {
+  axios.get(`${BASE_URL}/sales/amount-by-seller`)
+  .then(response => {
+      const data = response.data as SaleSum[];
+      const myLabels = data.map(x => x.sellerName);
+      const mySeries = data.map(x => x.sum);
+
+      chartData = { labels: myLabels, series: mySeries }
+      console.log(chartData);
+  });
+
+ /*  const mockData = {
     series: [477138, 499928, 444867, 220426, 473088],
     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-}
+} */
 
 const options = {
     legend: {
@@ -24,8 +37,8 @@ const options = {
 
   return (
       <Chart 
-        options={{ ...options, labels: mockData.labels}}
-        series={mockData.series}
+        options={{ ...options, labels: chartData.labels}}
+        series={chartData.series}
         type="donut"
         height="240"
       />
