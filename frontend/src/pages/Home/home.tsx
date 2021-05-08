bc6e539adcb2618ab4f8841b9b05e4feca73a5b0
danilo.queiroz <danilo.queiroz@crptecnologia.com.br>
2021-05-08 10:48:40 -0300

    Home, navigation

--- frontend/src/pages/Home/home.tsx
+++ frontend/src/pages/Home/home.tsx
@@ -1,10 +1,23 @@
+import Footer from "components/footer";
+import NavBar from "components/navbar";
+import { Link } from "react-router-dom";
 
 function Home() {
-    return (
-      <div>
-        <h1>Página Home</h1>
+  return (
+    <>
+    <NavBar />
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-4">DSVendas</h1>
+          <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+          <hr />
+          <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+        </div>
+        <Link to="/dashboard" className="btn btn-primary btn-lg"> Acessar Dashboard</Link>
       </div>
-    );
-  }
-  
-  export default Home;
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
