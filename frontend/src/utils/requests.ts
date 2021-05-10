d3adae8c4eac4835e4ea7a9995ea9f66347d6353
danilo.queiroz <danilo.queiroz@crptecnologia.com.br>
2021-05-10 17:10:44 -0300

    fixed link heroku in app react

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = process.env.BACKEND_URL ?? "http://localhost:8080";
+//export const BASE_URL = process.env.BACKEND_URL ?? "http://localhost:8080";
+export const BASE_URL = "https://sds3-queiroz.herokuapp.com" ?? "http://localhost:8080";
