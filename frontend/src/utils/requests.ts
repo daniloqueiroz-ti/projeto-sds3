65b10f4105b0d6afdb654febb7194402ba2b6196
danilo.queiroz <danilo.queiroz@crptecnologia.com.br>
2021-05-10 18:21:52 -0300

    Errata base url

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,3 +1,2 @@
-//variável encontra-se no servidor
-//BACKEND_URL = https://sds3-queiroz.herokuapp.com
-export const BASE_URL = process.env.BACKEND_URL ?? "http://localhost:8080";
+//export const BASE_URL = process.env.BACKEND_URL ?? "http://localhost:8080";
+export const BASE_URL = 'https://sds3-queiroz.herokuapp.com' ?? 'http://localhost:8080';
